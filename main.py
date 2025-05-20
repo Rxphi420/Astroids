be82eddbee31dad23a0c0c6479f66fdb7788d88d
Rxphi420 <Raphael.lederle@gmail.com>
2025-05-20 22:13:01 +0200

    E:Grouping

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ def main():
 	clock = pygame.time.Clock()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True :
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
-		player.update(dt)
+		updatable.update(dt)
 		screen.fill("black")
-		player.draw(screen)
+		for thing in drawable:
+		   thing.draw(screen)
+
 		pygame.display.flip()
 		dt= clock.tick(60) / 1000
 
